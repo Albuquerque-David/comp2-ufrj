c1ad87af91ad4e194f20c41c92aca3e8cd9a128c
David Albuquerque <davidra_rj@hotmail.com>
2021-03-27 23:32:50 -0300

    First commit in LAB1.

---
+++ LAB1/src/Primos.java
@@ -0,0 +1,104 @@
+public class Primos {
+
+    public static void main(String[] args) {
+        obterPrimos(4000);
+    }
+
+    /**
+     * Retorna um array de inteiros contendo todos os números primos (1,n) utilizando algoritmos auxiliares.
+     *
+     * @param n limite superior n do conjunto de números a ser procurado os números primos.
+     * @return int[] contendo os números primos entre 1 e n.
+     */
+    public static int[] obterPrimos(int n) {
+
+        /**
+         * Com Crivo de Erastótenes.
+         */
+        double t1 = System.currentTimeMillis();
+        int[] primosComCrivo = calculaPrimosComCrivo(40000000);
+        double t2 = System.currentTimeMillis();
+        double tf = (t2-t1)/1000;
+        System.out.printf("A execução usando o Crivo de Erastótenes demorou %.5f segundos.\n",tf);
+
+        /**
+         * Com método auxiliar.
+         */
+        t1 = System.currentTimeMillis();
+        t2 = System.currentTimeMillis();
+        tf = t2-t1;
+        System.out.printf("A execução usando um método auxiliar demorou %.10f segundos.\n",tf);
+
+        // printaPrimos(primosComCrivo);
+
+        return null;
+    }
+
+    /**
+     * Calcula e retorna um array de inteiros contendo todos os números primos (1,n).
+     *
+     * @param n limite superior n do conjunto de números a ser procurado os números primos.
+     * @return int[] contendo os números primos entre 1 e n.
+     */
+    public static int[] calculaPrimos(int n) {
+        int[] primos = new int[n];
+        return primos;
+    }
+
+    /**
+     * Calcula utilizando o Crivo de Erastótenes e retorna um array de inteiros contendo todos os números primos (1,n).
+     *
+     * @param n limite superior n do conjunto de números a ser procurado os números primos.
+     * @return int[] contendo os números primos entre 1 e n.
+     */
+    public static int[] calculaPrimosComCrivo(int n) {
+        boolean[] calcPrimos = new boolean[n+1];
+        int[] primos = new int[n+1];
+        int j = 0;
+
+        for(int i = 1; i <= n; i++){
+            calcPrimos[i] = true;
+        }
+
+        int cont = 2;
+        while(cont * cont <= n){
+
+            if(calcPrimos[cont] == true){
+                for(int i = cont*cont; i < n+1; i+=cont)
+                    calcPrimos[i] = false;
+            }
+
+            cont += 1;
+        }
+
+        calcPrimos[0] = false;
+        calcPrimos[1] = false;
+
+        for(int i = 0; i <= n; i++){
+            if(calcPrimos[i]){
+                primos[j] = i;
+                j++;
+            }
+
+        }
+
+        return primos;
+    }
+
+
+    /**
+     * Exibe na tela os números primos passados como parâmetro para a função.
+     *
+     * @param primos array de inteiros contendo os números primos.
+     */
+    public static void printaPrimos(int[] primos) {
+
+        for(int i = 0; i < primos.length; i++){
+            if(primos[i] == 0)
+                break;
+            System.out.println(primos[i]);
+        }
+
+    }
+
+}
